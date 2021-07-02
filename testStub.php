17e02825e474fbecf63f8b55b56e52364bd085ed
murata-bulky <41327622+murata-bulky@users.noreply.github.com>
2021-07-02 23:10:54 +0900

    Add files via upload

--- testStub.php
+++ testStub.php
@@ -1,8 +1,11 @@
 <?php
 	print("確認2");
+	echo '<br>';
 	print("ケースID：".$_GET["Id"]);
-	foreach (getallheaders() as $name => $value) {
-    	echo "$name: $value\n";
-    	
-	}
+	echo '<br>';
+	print("共通会員ID：".$_GET["InquiryCmnMmbrId__c"]);
+	echo '<br>';
+	print("共通会員IDハッシュ：".$_GET["InquiryCmnMmbrIdHash_7pay__c"]);
+	echo '<br>';
+	print("メールアドレス：".$_GET["Noemail__c"]);
 ?>
