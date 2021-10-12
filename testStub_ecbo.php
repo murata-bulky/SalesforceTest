db203a15fde5368db199a71ad21b35572be5d6d3
murata-bulky <41327622+murata-bulky@users.noreply.github.com>
2021-10-12 11:47:19 +0900

    Add files via upload

---
+++ testStub_ecbo.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>test</title>
+</head>
+<body>
+<?php
+	$caseId = "";
+	$commonMemberId = "";
+	$commonMemberIdHash = "";
+	$mailAddress = "";
+	$subMailAddress = "";
+	
+	print("ケースID：".$_GET["caseId"]);
+	echo '<br>';
+	print("共通会員ID：".$_GET["commonMemberId"]);
+	echo '<br>';
+	print("共通会員IDハッシュ：".$_GET["commonMemberIdHash"]);
+?>
+  <form action="https://contact-omni7--textecreb--c.visualforce.com/apex/ECBOtoCase" method="POST">
+  	
+  	<table>
+	    <tbody>
+	        <tr>
+	            <td>ケースID</td>
+	            <td><input type="text" name="caseId" value="<?php echo htmlspecialchars($caseId, ENT_QUOTES, 'UTF-8'); ?>"></td>
+	        </tr>
+	        <tr>
+	            <td>会員ID</td>
+	            <td><input type="text" name="commonMemberId" value="<?php echo htmlspecialchars($commonMemberId, ENT_QUOTES, 'UTF-8'); ?>"></td>
+	        </tr>
+	        <tr>
+	            <td>会員IDハッシュ</td>
+	            <td><input type="text" name="commonMemberIdHash" value="<?php echo htmlspecialchars($commonMemberIdHash, ENT_QUOTES, 'UTF-8'); ?>"></td>
+	        </tr>
+	        <tr>
+	            <td>メールアドレス</td>
+	            <td><input type="text" name="mailAddress" value="<?php echo htmlspecialchars($mailAddress, ENT_QUOTES, 'UTF-8'); ?>"></td>
+	        </tr>
+	        <tr>
+	            <td>サブメールアドレス</td>
+	            <td><input type="text" name="subMailAddress" value="<?php echo htmlspecialchars($subMailAddress, ENT_QUOTES, 'UTF-8'); ?>"></td>
+	        </tr>
+	    </tbody>
+	</table>
+    <input type="submit" value="submit">
+  </form>
+</body>
+</html>
